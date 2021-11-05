2457b05787d697667dd9110a0e97da4a89ebb14b
abhay <abhaybhagat2609@gmail.com>
2021-11-05 15:31:39 +0530

    commit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const input = document.getElementById("search");
-
+const result = document.getElementById("cardID");
+console.log(result);
+result.innerHTML = "";
+// https://abhaykumar007.github.io/SuperHero/
 // token :- 1699068923618895
 // "https://cors-anywhere.herokuapp.com/superheroapi.com/api/1699068923618895/search/name/";
 
@@ -10,10 +13,27 @@ async function getData() {
   const data = await response.json();
   // console.log(data);
 
-  var newData = data.filter(function (element) {
-    return element.name.toLowerCase().includes(input.value);
-  });
-  console.log(newData);
+  if (input.value.length == 0) {
+    result.innerHTML = "";
+  } else {
+    var newData = data.filter(function (element) {
+      return element.name.toLowerCase().includes(input.value);
+    });
+    console.log(newData);
+    newData.map((element, i) => {
+      result.innerHTML += `<ul class='result'">
+            <li class ='card'>
+              <img src='${element.images.md}' alt="" />
+              <div class="card-content">
+                <a class="content" href="">${element.name}</a>
+                <a class="content" href="">${element.biography.fullName}</a>
+                <a class="content" href="">Add to Favourites</a>
+              </div>
+            </li>
+          </ul>
+          <br />`;
+    });
+  }
 }
 function debounced(func, delay) {
   let timer;
