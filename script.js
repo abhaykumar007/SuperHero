115fe33f2112c715e5008e50878dc0a57d1ad259
abhay <abhaybhagat2609@gmail.com>
2021-11-05 17:44:31 +0530

    api

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 const input = document.getElementById("search");
 const result = document.getElementById("cardID");
-console.log(result);
+// console.log(result);
 result.innerHTML = "";
 // https://abhaykumar007.github.io/SuperHero/
 // token :- 1699068923618895
 // "https://cors-anywhere.herokuapp.com/superheroapi.com/api/1699068923618895/search/name/";
+// "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json"
 
 async function getData() {
-  const url =
-    "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json";
+  const inputVal = input.value;
+  const url = `https://www.superheroapi.com/api.php/1699068923618895/search/${inputVal}`;
   let response = await fetch(url);
   const data = await response.json();
   // console.log(data);
 
-  if (input.value.length == 0) {
-    result.innerHTML = "";
-  } else {
-    var newData = data.filter(function (element) {
-      return element.name.toLowerCase().includes(input.value);
-    });
-    console.log(newData);
+  const newData = data.results;
+  console.log(newData);
+  result.innerHTML = "";
+  if (inputVal.length > 0) {
     newData.map((element, i) => {
       result.innerHTML += `<ul class='result'">
             <li class ='card'>
-              <img src='${element.images.md}' alt="" />
+              <img src='${element.image.url}' alt="" />
               <div class="card-content">
                 <a class="content" href="">${element.name}</a>
                 <a class="content" href="">${element.biography.fullName}</a>
