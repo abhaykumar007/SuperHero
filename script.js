5a96256accd6b3b885d160597a940c0dda9c1089
abhay <abhaybhagat2609@gmail.com>
2021-11-05 13:17:36 +0530

    fetch done

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-// console.log("hello brother");
-const input = document.querySelector(".search");
-// console.log(input);
+const input = document.getElementById("search");
 
-const getData = (e) => {
-  console.log("get data", e);
-};
-// document.querySelector("input")
-input.addEventListener("keyup", (e) => {
-  let msg = e.target.value;
-  getData(msg);
-});
+// token :- 1699068923618895
+// "https://cors-anywhere.herokuapp.com/superheroapi.com/api/1699068923618895/search/name/";
+
+async function getData() {
+  const url =
+    "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json";
+  let response = await fetch(url);
+  const data = await response.json();
+  // console.log(data);
+
+  var newData = data.filter(function (element) {
+    return element.name.toLowerCase().includes(input.value);
+  });
+  console.log(newData);
+}
+function debounced(func, delay) {
+  let timer;
+  const debouncedFunction = () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      getData();
+    }, delay);
+  };
+  return debouncedFunction;
+}
+input.addEventListener("input", debounced(getData, 500));
